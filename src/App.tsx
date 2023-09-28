875752b709da1f8d8f994935de9ae0bc159e9905
JamesMokua <jamieedgar02@gmail.com>
2023-09-29 00:10:07 +0300

    finisher

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { ScrollControls } from "@react-three/drei";
-
+import {usePlay} from './contexts/Play.jsx'
+import { EffectComposer, Noise } from "@react-three/postprocessing";
+import { useMemo } from "react";
+import { Overlay } from "./components/Overlay";
 function App() {
+  const { play, end } = usePlay();
+
+  const effects = useMemo(
+    () => (
+      <EffectComposer>
+        <Noise opacity={0.02} />
+      </EffectComposer>
+    ),
+    []
+  );
+
   return (
     <>
       <Canvas>
         <color attach="background" args={["#ececec"]} />
-        <ScrollControls pages={100} damping={1}>
+        <ScrollControls   pages={play && !end ? 20 : 0}
+          damping={0.5}
+          style={{
+            top: "10px",
+            left: "0px",
+            bottom: "10px",
+            right: "10px",
+            width: "auto",
+            height: "auto",
+            animation: "fadeIn 2.4s ease-in-out 1.2s forwards",
+            opacity: 0,
+          }}>
             <Experience />
         </ScrollControls>
-      
+        {effects}
       </Canvas>
+      <Overlay />
     </>
   );
 }
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,32 +1,51 @@
 import { Environment, Sphere } from "@react-three/drei";
 import { Gradient, LayerMaterial } from "lamina";
 import * as THREE from "three";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
-const Background = () => {
-  const colorA = "#0923be";
-  const colorB = "#ffad30";
+const Background = ({backgroundColors}) => {
+  
   const start = 0.2;
   const end = -0.5;
+  const gradientRef =  useRef();
+  const gradientEnvRef = useRef();
+
+  useFrame(() => {
+    gradientRef.current.colorA = new THREE.Color(
+      backgroundColors.current.colorA
+    );
+    gradientRef.current.colorB = new THREE.Color(
+      backgroundColors.current.colorB
+    );
+    gradientEnvRef.current.colorA = new THREE.Color(
+      backgroundColors.current.colorA
+    );
+    gradientEnvRef.current.colorB = new THREE.Color(
+      backgroundColors.current.colorB
+    );
+  });
+
+  
 
   return (
     <>
       <Sphere scale={[500, 500, 500]} rotation-y={Math.PI / 2}>
         <LayerMaterial color={"#ffffff"} side={THREE.BackSide}>
           <Gradient
-            colorA={colorA}
-            colorB={colorB}
+          ref={gradientRef}
+            
             axes={"y"}
             start={start}
             end={end}
           />
         </LayerMaterial>
       </Sphere>
-      <Environment resolution={256} >
+      <Environment resolution={256} frames={Infinity} >
         <Sphere scale={[100, 100, 100]} rotation-y={Math.PI / 2} rotation-x={Math.PI}>
           <LayerMaterial color={"#ffffff"} side={THREE.BackSide}>
             <Gradient
-              colorA={colorA}
-              colorB={colorB}
+              ref={gradientEnvRef}
               axes={"y"}
               start={start}
               end={end}
